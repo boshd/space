3686af4b84079b40cec2245e398063f4ea85e339
Kareem Arab <kareemarab@cmail.carleton.ca>
2019-04-13 11:48:23 -0400

    added auth

--- back/api/api.go
+++ back/api/api.go
@@ -4,17 +4,14 @@ import "github.com/markcheno/go-vue-starter/models"
 
 // API object
 type API struct {
-	users  *models.UserManager
-	quotes *models.QuoteManager
+	users *models.UserManager
 }
 
 // NewAPI returns new api object
 func NewAPI(db *models.DB) *API {
 	usermgr, _ := models.NewUserManager(db)
-	quotemgr, _ := models.NewQuoteManager(db)
 
 	return &API{
-		users:  usermgr,
-		quotes: quotemgr,
+		users: usermgr,
 	}
 }
