40f538d592cc9ac941df263a1ecbb704d235394f
Kareem Arab <kareemarab@cmail.carleton.ca>
2019-04-14 17:40:19 -0400

    v

--- server.go
+++ server.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"github.com/kareemarab/space/api"
+	"github.com/kareemarab/space/models"
+
 	// "github.com/kareemarab/space/models"
 	"github.com/kareemarab/space/routes"
 	"github.com/urfave/negroni"
 )
 
 func main() {
-	// db := models.NewSqliteDB("data.db")
-	// api := api.NewAPI(db)
-	// routes := routes.NewRoutes(api)
-	// n := negroni.Classic()
-	// n.UseHandler(routes)
-	// n.Run(":3000")
-	fmt.Print("Hdd")
+	db := models.NewSqliteDB("data.db")
+	api := api.NewAPI(db)
+	routes := routes.NewRoutes(api)
+	n := negroni.Classic()
+	n.UseHandler(routes)
+	n.Run(":3000")
 }
