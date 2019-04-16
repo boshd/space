c713ad5b8ac24fb982be614459ad37f08d3c310e
Kareem Arab <kareemarab@cmail.carleton.ca>
2019-04-15 23:03:56 -0400

    re-structure

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	// create db connection
+	db, err = CreateConnection()
+	defer db.Close()
+
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	// will migrate User struct to database and will check for
+	// changes and will automatically migrate them everytime
+	// the service is started.
+
+	fmt.Print("jel")
+}
--- spaceios/Pods/Target Support Files/Pods-spaceiosTests/Pods-spaceiosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_spaceiosTests : NSObject
-@end
-@implementation PodsDummy_Pods_spaceiosTests
-@end
